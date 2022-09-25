c2f403c66b9c61acf3f1215197a2ba6b3cb40a1a
Vaibhav Vyas <vyasvaibhav03@gmail.com>
2022-09-25 15:51:06 +0530

    dynamicText - Done

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ let dynamicText = document.getElementById("dynamic-text");
 let phrases = ["Vaibhav Vyas...", "Software Engineer...", "Youtuber...","Gamer..."];
 let phraseIndex = 0;
 let letterIndex = 0;
-let typingSpeed = 150;
-let removingSpeed = 100;
+let typingSpeed = 100;
+let removingSpeed = 70;
 
 function printLetters(phrase){
-    if(letterIndex < phrase.length){
+
+    if(letterIndex == phrase.length){
+        clearLetters();
+    }
+
+    else if(letterIndex < phrase.length){
         dynamicText.textContent += phrase.charAt(letterIndex);
         letterIndex +=1;
         setTimeout(() => {
@@ -14,5 +19,21 @@ function printLetters(phrase){
         }, typingSpeed);
     }
 }
+function clearLetters(){
+    if(letterIndex == -1){
+        phraseIndex = (phraseIndex + 1) % phrases.length;
+        letterIndex = 0;
+        printLetters(phrases[phraseIndex])
+    }
+    else if(letterIndex > -1){
+        let updatedPhrase = "";
+        for(let index = 0; index<letterIndex; index++){
+            updatedPhrase += phrases[phraseIndex].charAt(index);
+        }
+        dynamicText.textContent = updatedPhrase;
+        letterIndex -=1;
+        setTimeout(clearLetters, removingSpeed);
+    }
+}
 
-printLetters(phrases[0]);
+printLetters(phrases[phraseIndex]);
